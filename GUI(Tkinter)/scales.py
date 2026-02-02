f8ad8bf7e0e6ca5bd1308b20f89f424ff7685180
SyedRihaanShah <rihaanshah766@gmail.com>
2026-02-02 19:15:36 +0530

    Modules

--- GUI(Tkinter)/scales.py
+++ GUI(Tkinter)/scales.py
@@ -1,23 +1,28 @@
-from  tkinter import *
+from tkinter import *
 
 def submit():
-    print("The temperature is " + str(scale.get()) + "degressC")
+    print("The Temperature is " + str(scale.get()))
 
 root = Tk()
-scale = Scale(root,
-              from_=0, to= 100,
+root.geometry("400x400") 
+
+scale = Scale(root, from_=0, to=100,
               length=500,
-              orient=VERTICAL,#orientation of the scale
-              font=("Consolas",20),
-              tickinterval=10,#this adds numeric indicators
-              showvalue=0,#hide current value
-              resolution=5,#increment of slider
-              troughcolor='#69FAFF',
-              fg='red',
-              bg='black')
-scale.set((scale['from'] - scale['to'])/2 + (scale['to']))
+              orient=VERTICAL, # horizontal or vertial
+              font=('consoloas', 20),
+              showvalue=0, #Hides current value
+             tickinterval= 10,#adds numeric 
+              resolution=5, # Increment of slider
+              troughcolor="#69FAFF", #Chnages the color of slider
+              bg='#111111', #changes the background color
+              fg='#FF1C00' # chnages the foreground or text color
+              )
+scale.set((scale['to']- scale['from'])/2)
 scale.pack()
 
-button = Button(root,text='Submit',command=submit)
+button = Button(root, text="Submit", command=submit)
 button.pack()
+
+
+
 root.mainloop()
